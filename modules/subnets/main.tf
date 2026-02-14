ec8af18d066a5f494d18cd916a99fd02f4a6e9f5
aqueelis <aqueel.kmit10@gmail.com>
2026-02-14 17:46:49 -0500

    subnets code commit

---
+++ modules/subnets/main.tf
@@ -0,0 +1,24 @@
+resource "aws_subnet" "public" {
+  count                   = length(var.public_subnet_cidrs)
+  vpc_id                  = var.vpc_id
+  cidr_block              = var.public_subnet_cidrs[count.index]
+  availability_zone       = var.availability_zones[count.index]
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "${var.name_prefix}-public-subnet-${count.index}"
+  }
+  
+}
+
+resource "aws_subnet" "private" {
+  count                   = length(var.private_subnet_cidrs)
+  vpc_id                  = var.vpc_id
+  cidr_block              = var.private_subnet_cidrs[count.index]
+  availability_zone       = var.availability_zones[count.index]
+  map_public_ip_on_launch = false
+
+  tags = {
+    Name = "${var.name_prefix}-private-subnet-${count.index}"
+  }
+}
