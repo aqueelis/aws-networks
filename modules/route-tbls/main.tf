034b58e2d7ba218717e935ed2edcaa76ff6b835f
aqueelis <aqueel.kmit10@gmail.com>
2026-02-14 18:36:43 -0500

    route table code commit

---
+++ modules/route-tbls/main.tf
@@ -0,0 +1,33 @@
+resource "aws_route_table" "public" {
+  vpc_id = var.vpc_id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = var.igw_id
+  }
+
+  tags = {
+    Name = "${var.name}-public-route-table"
+    resourceType = "tf"
+  }
+}
+
+resource "aws_route_table_association" "public" {
+  count          = length(var.public_subnet_ids)
+  subnet_id      = var.public_subnet_ids[count.index]
+  route_table_id = aws_route_table.public.id
+}
+
+resource "aws_route_table" "private" {
+  vpc_id = var.vpc_id
+
+  tags = {
+    Name = "${var.name}-private-route-table"
+  }
+}
+
+resource "aws_route_table_association" "private" {
+  count          = length(var.private_subnet_ids)
+  subnet_id      = var.private_subnet_ids[count.index]
+  route_table_id = aws_route_table.private.id
+}
